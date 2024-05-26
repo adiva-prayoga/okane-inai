bb445a68534a34ecdc3e6afbcd483c72d4c4304d
Adiva <prayoga.adiva@gmail.com>
2024-05-27 03:41:51 +0800

    :adhesive_bandage: Setup react proxy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Card,
   CardContent,
@@ -10,13 +10,26 @@ import "@/styles/App.css";
 
 function App() {
   const [totalSpent, setTotalSpent] = useState(0);
+
+  console.log(totalSpent);
+
+  useEffect(() => {
+    async function fetchTotal() {
+      const res = await fetch("/api/expenses/total-spent");
+      const data = await res.json();
+      setTotalSpent(data.total);
+    }
+
+    fetchTotal();
+  }, []);
+
   return (
-    <Card>
+    <Card className="w-80 m-auto text-left">
       <CardHeader>
         <CardTitle>Total Spent</CardTitle>
         <CardDescription>The total amount you've spent</CardDescription>
       </CardHeader>
-      <CardContent>{totalSpent}</CardContent>
+      <CardContent className="text-3xl">{totalSpent}</CardContent>
     </Card>
   );
 }
