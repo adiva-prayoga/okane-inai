c010717b86a91d6ac2d3ccdaf0a170da42479d70
Adiva <prayoga.adiva@gmail.com>
2024-06-10 18:27:23 +0800

    :card_file_box: Drizzle migration

--- drizzle/0000_motionless_peter_quill.sql
+++ drizzle/0000_motionless_peter_quill.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS "expenses" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"user_id" text NOT NULL,
 	"title" text NOT NULL,
-	"amount" numeric(12, 2) NOT NULL
+	"amount" numeric(12, 2) NOT NULL,
+	"date" date NOT NULL,
+	"created_at" timestamp DEFAULT now()
 );
 --> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "name_idx" ON "expenses" USING btree (user_id);
--- drizzle/0002_clammy_norman_osborn.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "expenses" ALTER COLUMN "created_at" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "expenses" ADD COLUMN "date" date NOT NULL;
